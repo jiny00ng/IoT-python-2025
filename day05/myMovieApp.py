7834df6451552bcea5775de47cc9ad71ff5123eb
JYK <jykim9226@gmail.com>
2025-02-10 10:11:02 +0900

    토이프로젝트 - 내 영화 앱

--- day05/myMovieApp.py
+++ day05/myMovieApp.py
@@ -24,8 +24,12 @@ def run():
         sel_menu = set_menu()
         if sel_menu == 1:
             # print('영화 입력')
-            movie = set_movie()
-            lst_movie.append(movie)
+            try:
+                movie = set_movie()
+                lst_movie.append(movie)
+                print('영화입력 성공!')
+            except Exception as e:
+                print(f'영화입력 실패!! {e}')
 
         elif sel_menu == 2:
             print('영화 출력')
@@ -54,9 +58,14 @@ def run():
 
 # 영화검색 함수
 def search_movie(items: list, title: str):
+    count = 0
     for item in items: # item이 Movie 클래스인지 알 수 없음
         if item.isNameContain(title): # 오타발생 위험!
+            count += 1 # 검색된 결과가 있음
             print(item)
+            print('----------')
+    
+    print(f'검색 데이터 수 : {count} 개')
 
 def del_movie(items: list, title: str):
     for i, item in enumerate(items):
@@ -106,7 +115,9 @@ def set_movie():
 def get_movie(items: list):
     for item in items: 
         print(item) # Movie 객체
-    
+        print('----------') # 각 영화 아이템별 구분자
+    print(f'총 데이터 수: {len(items)} 개')
+
 def set_menu():
     str_menu = (f'내 영화 앱 v{VERSION}\n'
                 '1. 영화 입력\n'
